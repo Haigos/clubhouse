1f27994389630cbe1a100bf4f8469ee63842148c
haigos <timzurb@gmail.com>
2022-06-13 13:50:55 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
+// styles
 import './App.css'
+
+// react router
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+// pages
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Signup from './pages/signup/Signup'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
 
+            <Route exact path='/signup'>
+              <Signup />
+            </Route>
+
+            <Route exact path='/login'>
+              <Login />
+            </Route>
+
+            <Route exact path='/project/:id'>
+              <Project />
+            </Route>
+
+            <Route exact path='/create'>
+              <Create />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App
+
